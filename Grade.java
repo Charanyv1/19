95359ae174290d3d62ce7a61ed8dc2ae9cd1127a
Charanyv1 <charanyv31@gmail.com>
2026-04-22 21:21:47 +0530

    Create Grade.java

---
+++ Grade.java
@@ -0,0 +1,17 @@
+class Grade {
+    public static void main(String[] args) {
+        int marks = 85;  // change this value to test
+
+        if (marks >= 90) {
+            System.out.println("Grade: A");
+        } else if (marks >= 75) {
+            System.out.println("Grade: B");
+        } else if (marks >= 60) {
+            System.out.println("Grade: C");
+        } else if (marks >= 50) {
+            System.out.println("Grade: D");
+        } else {
+            System.out.println("Grade: F");
+        }
+    }
+}
